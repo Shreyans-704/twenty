1105f3696b4cccbfda0a4335b1df707399476090
Shreyans Jaiswal <shreyansj.it.23@nitj.ac.in>
2026-04-11 00:42:34 +0530

    fix: use config service with fallback for NODE_PORT

--- packages/twenty-server/src/main.ts
+++ packages/twenty-server/src/main.ts
@@ -79,7 +79,7 @@ const bootstrap = async () => {
   // Inject the server url in the frontend page
   generateFrontConfig();
 
-  await app.listen(process.env.NODE_PORT || 3000);
+  await app.listen(twentyConfigService.get('NODE_PORT') || 3000);
 };
 
 bootstrap();
